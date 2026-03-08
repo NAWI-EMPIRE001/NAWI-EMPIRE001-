1c1fde4dbe34fdf72d014ccd58197e82b3343854
NAWI-EMPIRE <akpanvictor848@gmail.com>
2026-03-09 00:46:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// This tells the platform to show your HTML, CSS, and images
 app.use(express.static(__dirname));
 
-// This serves your Luxury Mall entrance (index.html)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// This starts the Empire's engine
 app.listen(PORT, () => {
-    console.log(`NAWI-EMPIRE is now Live on Port ${PORT}`);
+    console.log('NAWI-EMPIRE Engine Active');
 });
