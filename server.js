ec0fa38b78ab399b029f20a66082c211a559d530
NAWI-EMPIRE <akpanvictor848@gmail.com>
2026-03-11 00:54:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-Const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://akpanvictor848_db_user:NAWI-EMPIRE@nawi-empire.3qj9wnj.mongodb.net/?appName=NAWI-EMPIRE";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a NAWI-EMPIRE ClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true,
+    deprecationgood: true,
   }
 });
 
@@ -18,14 +18,8 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // Ensures that the client will close when you finish/good 
+    Colin await 
   }
 }
-run().catch(console.dir);
-
-
-
-
-This is the password: NAWI-EMPIRE 
-That's my mangodb code please check it well before we paste it on the Javajs
+run().catch(console.dir); 7 pillars 
