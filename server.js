92c9b6b58863bec159b92e600372b02e0dd83d32
NAWI-EMPIRE <akpanvictor848@gmail.com>
2026-03-11 07:39:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://akpanvictor848_db_user:NAWI-EMPIRE@nawi-empire.3qj9wnj.mongodb.net/?appName=NAWI-EMPIRE";
 
-// Create a MongoClient with a NAWI-EMPIRE ClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationgood: true,
+    deprecationErrors: true,
   }
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/good 
-    Colin await 
+    
+    // THE SYSTEM IS NOW LIVE. DO NOT ADD 'CLOSE' COMMANDS HERE.
+    
+  } catch (error) {
+    console.error("Connection failed:", error);
   }
 }
-run().catch(console.dir); 7 pillars 
+
+// Start the Engine
+run().catch(console.dir);
